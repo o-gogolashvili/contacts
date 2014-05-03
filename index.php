10c3970e4d700a8d89a7a70f62306e9158ccc454
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-05-03 12:05:08 +0000

    Bootstrap files linked

--- index.php
+++ index.php
@@ -4,26 +4,27 @@
         <meta charset="utf-8">
         <title> Contacts </title>
         <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
         <script src="js/bootstrap.js"></script>
     </head>
     <body>
         <div>
             <form action="" method="post">
-                <input type="text" placeholder="Username"><br/>
-                <input type="password" placeholder="Password"><br/>
-                <input type="submit" value="signIn"><hr/>
+                <input type="text" placeholder="Username"/><br/>
+                <input type="password" placeholder="Password"/><br/>
+                <input type="submit" value="signIn"/><hr/>
             </form>
         </div>
         <div>
             <form action="" method="post">
-                <input type="text" placeholder="Enter Desired Username"><br/>
-                <input type="password" placeholder="Enter Password"><br/>
-                <input type="password" placeholder="Retype Password"><br/>
-                <input type="text" placeholder="First Name"><br/>
-                <input type="text" placeholder="Last Name"><br/>
-                <input type="text" placeholder="Phone Number"><br/>
-                <input type="text" placeholder="Photo URL"><br/>
-                <input type="submit" value="signUp">
+                <input type="text" placeholder="Enter Desired Username"/><br/>
+                <input type="password" placeholder="Enter Password"/><br/>
+                <input type="password" placeholder="Retype Password"/><br/>
+                <input type="text" placeholder="First Name"/><br/>
+                <input type="text" placeholder="Last Name"/><br/>
+                <input type="text" placeholder="Phone Number"/><br/>
+                <input type="text" placeholder="Photo URL"/><br/>
+                <input type="submit" value="signUp"/>
             </form>
         </div>
     </body>
