8118264e8a5c4580f372b5c6d806be282cfe486a
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-05-03 11:41:28 +0000

    Added bootstrap

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title> Contacts </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
         <div>
