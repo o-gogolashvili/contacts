332beaf86cc94fa758fc292e404c0a286530a88f
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-05-03 10:36:21 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Contacts </title>
+    </head>
+    <body>
+        <div>
+            <form action="" method="post">
+                <input type="text" placeholder="Enter Desired Username">
+                <input type="password" placeholder="Enter Password">
+                <input type="password" placeholder="Retype Password">
+                <input type="text" placeholder="First Name">
+                <input type="text" placeholder="Last Name">
+                <input type="text" placeholder="Phone Number">
+                <input type="text" placeholder="Photo URL">
+                <input type="submit" value="signUp">
+            </form>
+        </div>
+    </body>
+</html>
