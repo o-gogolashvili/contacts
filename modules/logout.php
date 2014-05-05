6ca61aa0a17f8e70078ff402635a21976e280f75
Otar Gogolashvili <otar@Omedia-Staging.(none)>
2014-05-05 11:36:56 +0000

    updated signup.php, home.html and added logout.php

---
+++ modules/logout.php
@@ -0,0 +1,8 @@
+﻿<?php
+ 
+	$_SESSION[] = array();
+	session_destroy();
+
+	header('location: /index.php');
+
+?>
--- modules/signup.php
+++ modules/signup.php
@@ -1,13 +1,34 @@
-<?php
+﻿<?php
 
 	$user = new user();
+	if (!empty($_POST['mail'])) {
 	$user->setMail($_POST['mail']);
+	} else { $message[] = 'Enter your mail';
+	}
+	if (!empty($_POST['firstName'])) {
 	$user->setFirstName($_POST['firstName']);
+	} else { $message[] = 'Enter your first name';
+	}
+	if (!empty($_POST['lastName'])) {
 	$user->setLastName($_POST['lastName']);
-	$user->setPassword($_POST['password']);
-	$user->setRegDate(date('Y-m-d'));
+	} else { $message[] = 'Enter your last name';
+	}
+	if(empty($password)) {
+      $message[] = 'Enter password';
+    } elseif($password != $re_password) {
+      	$message[] = 'Passwords do not match';
+    } else {
+    	$user->setPassword($_POST['password']);
+    }
+	$user->setRegDate(date('Y-m-d H:I:s'));
 	
-	$user->addUser();
+	if (empty($message)) {
+		$user->addUser();
+	} else {
+		for_each ($message as $comment) {
+			print ($comment.'<br />');
+		}
+	}
 
 	print('Registration Successful! Now you can Sign In');
 
