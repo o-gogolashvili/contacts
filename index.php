c7e9556ca57ac1a022aade9471aa5d30c77c7594
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-05-05 11:13:54 +0000

    Added sessions

--- index.php
+++ index.php
@@ -1,43 +1,34 @@
-<?php
-    
-    require('models/user.php');
-
-    if(isset($_POST['signUp'])) {
-        require('modules/config.php');
-        require('modules/connect.php');
-        require('modules/signup.php');
-    }
-    if(isset($_POST['signIn'])) {
-        //აქ დასამატებელია აუთენტიკაცია
-    }
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title> Contacts </title>
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.css">
-        <script src="js/bootstrap.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div>
-            <form action="" method="post">
-                <input name="mail" type="text" placeholder="E-Mail"/><br/>
-                <input name="password" type="password" placeholder="Password"/><br/>
-                <input name="signIn" type="submit" value="signIn"/><hr/>
-            </form>
-        </div>
-        <div>
-            <form action="" method="post">
-                <input name="mail" type="text" placeholder="E-mail"/><br/>
-                <input name="firstName" type="text" placeholder="First Name"/><br/>
-                <input name="lastName" type="text" placeholder="Last Name"/><br/>
-                <input name="password" type="password" placeholder="Password"/><br/>
-                <input name="signUp"type="submit" value="signUp"/>
-            </form>
-        </div>
+<?php
+    
+    require('models/user.php');
+    require('modules/config.php');
+    require('modules/connect.php');
+    
+    session_start();
+
+        if(!empty($_SESSION['mail'])) {
+            print('Logged in with <b>'.$_SESSION['mail'].'</b>');
+        }
+        else {
+            require('pages/home.html');
+        }
+
+    if(isset($_POST['signUp'])) {
+        require('modules/signup.php');
+    }
+    if(isset($_POST['signIn'])) {
+        require('modules/signin.php');
+    }
+?>
     </body>
 </html>
