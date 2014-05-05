bf4abb97ca0dc1d23e1edef2a0fd84ce4f21d53b
Otar Gogolashvili <otar@Omedia-Staging.(none)>
2014-05-05 10:45:35 +0000

    updated connect.php

--- modules/connect.php
+++ modules/connect.php
@@ -1,6 +1,14 @@
 <?php
 
-  mysql_connect($config['host'] , $config['db_user'] , $config['db_pass']);
-  mysql_select_db($config['db_name']);
-  
+    if (mysql_connect($config['host'] , $config['db_user'] , $config['db_pass'])) {
+    if (mysql_select_db($config['db_name'])) {
+        // nothing here ;)
+    	} else {
+        print "Couldn't select database";
+        exit();
+    	}
+    } else {
+    print "Couldn't connect to database";
+    exit();
+    }
 ?>
