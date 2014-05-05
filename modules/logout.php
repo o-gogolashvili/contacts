d0e945e9ffb2b6b73c1388aea98872cfcc928709
Otar Gogolashvili <otar@Omedia-Staging.(none)>
2014-05-05 10:34:17 +0000

    added logout.php and signin.php is fixed

---
+++ modules/logout.php
@@ -0,0 +1,8 @@
+<?php
+
+  $_SESSION[] = array();
+  session_destroy();
+
+  header('location: /index.php');
+
+?>
--- modules/signin.php
+++ modules/signin.php
@@ -1,17 +1,16 @@
 ﻿<?php
 
 	session_start();
-	print $_POST['mail'];
-	print md5($_POST['password']);
+	
 	if (!empty($_POST)) {
 		$query = 'select * from contacts where mail="'.$_POST['mail'].'" and password="'.md5($_POST['password']).'"';
 		$result = mysql_query($query);
-		$user=mysql_fetch_array();
+		$user=mysql_fetch_array($result);
 		if (!empty($user)) {
 			$_SESSION['mail'] = $user['mail'];
 			$_SESSION['password'] = $user['password'];
 			$_SESSION['id'] = $user['id'];
 		} else print ('Incorrect Details');
 	}
-	  
+	 
 ?>
