e4c741fde1f3644fd2aeb0412ddaeaee422620ba
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-05-09 17:01:25 +0000

    user content page stylized for future use

--- pages/usercontent.php
+++ pages/usercontent.php
@@ -1,5 +1,22 @@
-<?php
-
-	print('Logged in with <b>'.$_SESSION['mail'].'</b> <a href="index.php?action=logout"> Log Out </a>');
-
-?>
+<div class="container">
+    <div class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"> CONTACTS </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                	<li>
+                		<a class="btn" href="index.php?action=logout">Log Out</a>
+                	</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="jumbotron">
+	        <?php
+				print('Logged in with <b>'.$_SESSION['mail'].'</b>');
+	            //user content modules here
+	        ?>
+    </div>
+</div>
