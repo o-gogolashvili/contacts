4341db494d8fe04e5a0d3b99e916bc59a019b40c
Otar Gogolashvili <otar@Omedia-Staging.(none)>
2014-05-20 17:22:51 +0000

    some problems were fixed

--- modules/addcontact.php
+++ modules/addcontact.php
@@ -1,19 +1,20 @@
 <div class="container">
 	<div class="jumbotron">
-		<form class="navbar-form navbar-right" role="form" action="" method="post">
+		<form class="form-signup" action="" method="post" role="form">
 			<input class="form-control" name="firstName" type="text" placeholder="First Name" />
 			<input class="form-control" name="lastName" type="text" placeholder="Last Name" />
 			<input class="form-control" name="mobilePhone" type="number" placeholder="Mobile Phone" />
 			<input class="form-control" name="homePhone" type="number" placeholder="Home Phone" />
 			<input class="form-control" name="officePhone" type="number" placeholder="Office Phone" />
-			<input class="form-control" name="mobilePhone" type="number" placeholder="Mobile Phone" />
 			<input class="form-control" name="birthDate" type="date" placeholder="Birth Date" />
-			<input class="form-control" name="mobilePhone" type="number" placeholder="Mobile Phone" />
-			<input class="form-control" type="radio" name="sex" value="1" /> Male
+			<label class="radio-inline">
+				<input class="form-control" type="radio" name="sex" value="1" /> Male</label>
+			<label class="radio-inline">
 			<input class="form-control" type="radio" name="sex" value="0" /> Female
+			</label>
 			<input class="form-control" name="job" type="text" placeholder="Job" />
-			<input class="form-control" name="facebook" type="url" placeholder="Facebook" />
-			<input class="form-control" name="twitter" type="url" placeholder="Twitter" />
+			<input class="form-control" name="facebook" type="text" placeholder="Facebook" />
+			<input class="form-control" name="twitter" type="text" placeholder="Twitter" />
 			<input class="btn btn-success" name="addContact" type="submit" value="Add Contact"/>
 		</form>
 	</div>
@@ -21,7 +22,7 @@
 
 <?php
 	
-	require('models/contacts.php');
+	//require('models/contacts.php');
 
 	$contact = new contacts();
 
