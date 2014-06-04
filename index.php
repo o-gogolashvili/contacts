b863ccb924896202dd3850a078f8ec37345a5059
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-06-04 21:05:34 +0000

    added delete button and tooltips

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
                 }
         ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>				
+        <script src="js/bootstrap.min.js"></script>	
+        <script src="js/global.js"></script>
     </body>
 </html>
--- modules/contactlist.php
+++ modules/contactlist.php
@@ -11,15 +11,18 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h4 class="panel-title">
-					<a data-toggle="collapse" data-parent="#accordion" href="#<?php print($contact['contact_id']); ?>">
+					<a data-toggle="collapse" data-parent="#accordion" href="#<?php print("cntlistitem".$contact['contact_id']); ?>">
 						<?php print($contact['contact_last_name'].' '.$contact['contact_first_name']); ?>
 					</a>
-					<a class="" href="index.php?action=editcontact&id=<?php print($contact['contact_id']); ?>">
+					<a class="btn btn-default" data-toggle="tooltip" data-placement="top" data-original-title="Edit" href="index.php?action=editcontact&id=<?php print($contact['contact_id']); ?>">
 						<span class="glyphicon glyphicon-pencil"></span>
+					</a>
+					<a class="btn btn-default" data-toggle="tooltip" data-placement="top" data-original-title="Delete" href="index.php?action=delete?id=<?php print($contact['contact_id']); ?>">
+						<span class="glyphicon glyphicon-trash"></span>
 					</a>
 				</h4>
 			</div>
-			<div id="<?php print($contact['contact_id']); ?>" class="panel-collapse collapse">
+			<div id="<?php print("cntlistitem".$contact['contact_id']); ?>" class="panel-collapse collapse">
 				<div class="panel-body">
 					<?php
 						if($contact['sex']==1) {
