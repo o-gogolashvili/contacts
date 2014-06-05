d3ef6e8b60bc029fcc80def980b971d10d9e9d6a
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-06-05 20:14:08 +0000

    null

--- js/global.js
+++ js/global.js
@@ -1,19 +1,5 @@
 $('[data-toggle="tooltip"]').tooltip(); // activates tooltips
 
 $('[name="searchTerm"]').on('input', function() {
-
-	var searchTerm = $('[name="searchTerm"]').val();
-	alert(searchTerm);
-	
-
-	/*
-
-	$POST();
-
-	$.ajax({
-		type: "POST",
-		url: "modules/contactlist.php",
-		data: $('[name="searchTerm"]').val(),
-		success: success,
-	}); */
+	//ajax
 });
