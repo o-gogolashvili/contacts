058647fdb548032468f461df3ebc12a2e7b04fa7
Giorgi Botsvadze <g_bocvadze@cu.edu.ge>
2014-06-05 11:50:15 +0000

    added javaScript code, needs finish

--- js/global.js
+++ js/global.js
@@ -1,1 +1,19 @@
 $('[data-toggle="tooltip"]').tooltip(); // activates tooltips
+
+$('[name="searchTerm"]').on('input', function() {
+
+	var searchTerm = $('[name="searchTerm"]').val();
+	alert(searchTerm);
+	
+
+	/*
+
+	$POST();
+
+	$.ajax({
+		type: "POST",
+		url: "modules/contactlist.php",
+		data: $('[name="searchTerm"]').val(),
+		success: success,
+	}); */
+});
